e983421fd7f134042a392571f3abca7cab22264c
lterentyeva <lterentyeva@yandex.ru>
2022-03-20 14:47:03 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Входные данные
+        int cost = 200;
+        int amount = 20;
+
+
+        // Логика программы
+        int bonus = cost / amount;
+
+        System.out.println("Ваш бонус составит:" + bonus);
+    }
+}
